76041345a88a4581903042c2834cfc270c073cc0
luismartinezti <luis.martinez@institutojassa.edu.mx>
2026-04-21 13:26:24 -0600

    Update cambiofondo.bat

--- cambiofondo.bat
+++ cambiofondo.bat
@@ -1,18 +1,16 @@
 @echo off
 
-:: URL de la imagen
 set URL=https://img2.wallspic.com/previews/4/3/5/7/97534/97534-naturaleza-bosque-la_vegetacion-ecosistema-los_bosques_de_viejo_crecimiento-x750.jpg
+set FILE=C:\wallpaper.jpg
 
-:: Ruta donde se guardará
-set FILE=%TEMP%\wallpaper.jpg
+:: Descargar imagen con TLS
+powershell -ExecutionPolicy Bypass -command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '%URL%' -OutFile '%FILE%'"
 
-:: Descargar imagen (PowerShell)
-powershell -command "Invoke-WebRequest -Uri '%URL%' -OutFile '%FILE%'"
+:: Limpiar cache
+del /f /q "%APPDATA%\Microsoft\Windows\Themes\TranscodedWallpaper" 2>nul
+del /f /q "%APPDATA%\Microsoft\Windows\Themes\CachedFiles\*" 2>nul
 
-:: Cambiar fondo de pantalla
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%FILE%" /f
-
-:: Aplicar cambios
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+:: Aplicar fondo (versión simple)
+powershell -ExecutionPolicy Bypass -command "(Add-Type '[DllImport(\"user32.dll\")]public static extern bool SystemParametersInfo(int uAction,int uParam,string lpvParam,int fuWinIni);' -Name a -Pas)::SystemParametersInfo(20,0,'%FILE%',3)"
 
 exit
