db9c5e5e2601c0b78d8d9510e8704ac44653b2cd
luismartinezti <luis.martinez@institutojassa.edu.mx>
2026-04-21 13:04:53 -0600

    Update cambiofondo.bat

--- cambiofondo.bat
+++ cambiofondo.bat
@@ -1,22 +1,18 @@
 @echo off
 
+:: URL de la imagen
 set URL=https://img2.wallspic.com/previews/4/3/5/7/97534/97534-naturaleza-bosque-la_vegetacion-ecosistema-los_bosques_de_viejo_crecimiento-x750.jpg
-set FILE=C:\wallpaper.jpg
 
-:: Descargar imagen
+:: Ruta donde se guardará
+set FILE=%TEMP%\wallpaper.jpg
+
+:: Descargar imagen (PowerShell)
 powershell -command "Invoke-WebRequest -Uri '%URL%' -OutFile '%FILE%'"
 
-:: Borrar cache
-del /f /q "%APPDATA%\Microsoft\Windows\Themes\TranscodedWallpaper"
-del /f /q "%APPDATA%\Microsoft\Windows\Themes\CachedFiles\*" 2>nul
+:: Cambiar fondo de pantalla
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%FILE%" /f
 
-:: Aplicar fondo
-powershell -command "Add-Type @'
-using System.Runtime.InteropServices;
-public class Wallpaper {
-[DllImport(\"user32.dll\",SetLastError=true)]
-public static extern bool SystemParametersInfo(int uAction,int uParam,string lpvParam,int fuWinIni);
-}
-'@; [Wallpaper]::SystemParametersInfo(20,0,'%FILE%',3)"
+:: Aplicar cambios
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 
 exit
